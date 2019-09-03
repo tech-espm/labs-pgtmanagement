e2e58cfe0d6798a4c846304b6de436a1f7b7e5aa
Lelexg <30670086+Lelexg@users.noreply.github.com>
2019-09-02 21:39:01 -0300

    Add files via upload

---
+++ PGTManagement/AppSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PGTManagement
+{
+    public class AppSetting
+    {
+        public string StorageConnectionString { get; set; }
+        public string PGTData { get; set; }
+    }
+}
